c1cc25dd1e7775ca4c8f451badd4bc7101bcd73c
Savita <82285804+savita-star@users.noreply.github.com>
2024-05-03 17:59:00 +0000

    third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,7 @@
-// Create Web Server
-// Create a web server that's going to send a response of "Hello World" for every request it receives. The server should listen on port 3000.
-
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World');
-}).listen(3000);
+// Create web server
+// Create comments
+// Create comments.js
+// Create comments.html
+// Create comments.css
+// Create comments.js
+// Create comments.json
